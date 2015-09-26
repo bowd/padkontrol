23a7a7e448f63f4151fbefa700fa62c8a74f795c
Bogdan Dumitru <dumbogdan@gmail.com>
2015-09-26 12:12:07 +0300

    Basic sequencer layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _polyfill from 'babel-core/polyfill';
 import React from 'react';
 import App from './views/app';
 import normalize from 'normalize.css';
---
+++ src/views/sequencer/bar/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Bar extends Component {
+  render() {
+    return (
+      <span> Bar </span>
+    )
+  }
+}
