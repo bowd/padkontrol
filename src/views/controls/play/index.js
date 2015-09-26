b5b813143dc49a35dfc33931b1b1825e39108c3c
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2015-09-26 17:05:53 +0300

    play button

--- src/views/controls/play/index.js
+++ src/views/controls/play/index.js
@@ -20,11 +20,12 @@ export default class Play extends Component {
   }
 
   render() {
+    var className = 'PlayButton ' + (this.props.playing ? 'active' : '' );  
     return (
-      <a href="#" className="PlayButton playing" onClick={this.togglePlay}>
+      <button className={className} onClick={this.togglePlay}>
         <i className="fa fa-play play"></i>
         <i className="fa fa-stop stop"></i>
-      </a>
+      </button>
     )
   }
 }
