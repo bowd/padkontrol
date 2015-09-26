e1414fd6d88e15bf5c84ebfd56d0c7cb1bb6e987
Bogdan Dumitru <dumbogdan@gmail.com>
2015-09-26 16:47:56 +0300

    Listen to playing

--- src/views/controls/play/index.js
+++ src/views/controls/play/index.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'lib/flux';
 import SequencerActionCreators from 'action_creators/sequencer';
 import style from './index.css';
+let { bool } = PropTypes;
 
+@connect({
+  interests: {
+    'global.playing': 'playing'
+  }
+})
 export default class Play extends Component {
+  static propTypes = {
+    playing: bool
+  }
+
   togglePlay = () => {
     SequencerActionCreators.togglePlay();
   }
