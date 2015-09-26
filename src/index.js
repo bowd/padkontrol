c29c9c60651bac6156ad20d683f099d098207f95
Bogdan Dumitru <dumbogdan@gmail.com>
2015-09-26 17:05:49 +0300

    Add FastClick

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import _polyfill from 'babel-core/polyfill';
 import React from 'react';
 import App from './views/app';
 import normalize from 'normalize.css';
+import FastClick from 'fastclick';
 
 import stores from './stores';
 import { initStateTree } from 'lib/flux';
 
 let stateString = localStorage.getItem('state');
 let state = stateString !== null ? JSON.parse(stateString) : {};
+
+FastClick.attach(document.body);
 
 initStateTree(stores, state);
 React.render(<App />, document.getElementById('root'));
